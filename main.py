13255b3b87371f11455e379af2a15e9c25df41dd
miaozai1 <pyin748@aucklanduni.ac.nz>
2022-08-05 16:23:46 +0800

    Updatad console outputs.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print('Hello World!')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Hello World!, Git is useful!')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
